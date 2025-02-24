420d7b260ca3951685c7cafd4249ab416885826d
Meetpidev <meetkshah3112@gmail.com>
2025-02-24 21:41:41 +0530

    added

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,8 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, {
-    });
+    await mongoose.connect(process.env.DB_URL, {});
     console.log('MongoDB connected...');
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
